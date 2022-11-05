bb6b0628b14f472f075c18e9fb358ea0d0b0162d
SIFAT69 <sifatkazi69@gmail.com>
2022-11-05 21:53:11 +0600

    Yo

---
+++ includes/config.php
@@ -0,0 +1,12 @@
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'ecommerce');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+if (mysqli_connect_errno())
+{
+	echo "Unable to connect with the database. " . mysqli_connect_error();
+}
+?>
